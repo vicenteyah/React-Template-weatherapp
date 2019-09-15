5891f741b03469de4cd57b90b0b41b6723017082
Vicente Yah <vicente.yah.dzul@icloud.com>
2019-09-14 21:58:28 -0500

    importing styles.css from WeatherLocation and also create a new class to use

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Location from "./Location";
 import WeatherData from "./WeatherData";
+import './styles.css';
+
 const WeatherLocation = ()=>(
 
-    <div>
+    <div className = "weatherLocationCont">
         <Location city ={"Mérida"}></Location>
         <WeatherData></WeatherData>
     </div>
