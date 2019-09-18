00b4d1859641bf407294b9634c8397933c589d1c
Vicente Yah <vicente.yah.dzul@icloud.com>
2019-09-17 21:48:09 -0500

    Proptypes validation implemented

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
 import './styles.css'; 
@@ -12,4 +13,13 @@ const WeatherData = ({data:{temperature, weatherState,humidity,wind}}) => (
         <WeatherExtraInfo humidity = {humidity} wind ={wind}/>
     </div>
 );
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired
+    })
+}
 export default WeatherData;
