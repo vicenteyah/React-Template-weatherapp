3dd534029cd47eaa2bdb7e0c216c657a125abc36
vicenteyah <vicente.yah.dzul@icloud.com>
2019-09-17 23:43:59 -0500

    button and new const was created to use events

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,7 +4,7 @@ import WeatherData from "./WeatherData";
 import './styles.css';
 
 import {
-    SUN,
+    SUN, WINDY,
 } from './../../constans/weathers';
 
 const data ={
@@ -13,13 +13,39 @@ const data ={
     humidity: 20,
     wind:'10m/s'
 }
+const data2 = {
+    temperature: 18,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '10m/s'
+}
 
 class WeatherLocation extends Component{
+    /*Constructor*/
+    constructor (){
+        super();
+        this.state = {
+            city: 'Mérida',
+            data: data,
+        };
+    }
+    handleUpdateClick =()=>{
+        console.log("refreshing......>:v")
+        this.setState({
+            city: 'Mérida!',
+            data: data2
+            
+        });
+ 
+    }
     render(){
+        const { city, data} = this.state;
         return(
             <div className = "weatherLocationCont">
-                <Location city ={"Mérida"}></Location>
+                <Location city ={city}></Location>
                 <WeatherData data ={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Refresh</button>{/* onClick button is doing refernete to handleupdateClick
+                to see if the event is working*/}
             </div>
         );
     }
