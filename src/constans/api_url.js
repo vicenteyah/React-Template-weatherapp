698b323927662659b558003732eca731896cada4
vicenteyah <vicente.yah.dzul@icloud.com>
2019-09-22 12:14:59 -0500

    consts api_key and url-base-weather were exported

--- src/constans/api_url.js
+++ src/constans/api_url.js
@@ -1,5 +1,4 @@
-const location = "Merida,MX";
-const api_key = "449b543c7605df17ade6147db618b87c";
-const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 
-export const api_weather = `${url_base_weather}?q=${location}&APPID=${api_key}`;
+export const api_key = "449b543c7605df17ade6147db618b87c";
+export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
